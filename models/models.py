61048ebaed99264a8a45672ae148490d983f0e2f
psat6 <psat6@penguin>
2021-02-26 12:28:45 +0100

    1.3

--- models/models.py
+++ models/models.py
@@ -2,6 +2,6 @@
 
 from odoo import models, fields, api
 
-class hr.employee(models.Model):
+class hremployee(models.Model):
     _inherit = 'hr.employee'
     user_id = fields.Many2one('hr.employee', string='Sales', default=lambda self: self.env.user)
