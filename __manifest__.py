b4b80be61a5307c829318aee27b9ebbdd5bec3d6
psat6 <psat6@penguin>
2021-02-26 10:06:53 +0100

    1a versio

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "filtro_comercial",
+    'name': "Filtro comercial",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -20,16 +20,9 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+    'depends': ['base', 'hr'],
+    
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        'views/inherited_form_view.xml',
     ],
 }
--- models/models.py
+++ models/models.py
@@ -2,14 +2,6 @@
 
 from odoo import models, fields, api
 
-# class filtro_comercial(models.Model):
-#     _name = 'filtro_comercial.filtro_comercial'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class hrdepartment(models.Model):
+    _inherit = 'hr.department'
+    user_id = fields.Many2one('hr.department', string='Salesperson', index=True, track_visibility='onchange', default=lambda self: self.env.user)
