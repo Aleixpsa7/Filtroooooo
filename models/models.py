27d62b08cdb0351d299d9beb4b164f8c4d9f4e4b
psat6 <psat6@penguin>
2021-02-26 10:18:45 +0100

    1.1 - sales en comptes de salesperson

--- models/models.py
+++ models/models.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api
 
 class hrdepartment(models.Model):
     _inherit = 'hr.department'
-    user_id = fields.Many2one('hr.department', string='Salesperson', index=True, track_visibility='onchange', default=lambda self: self.env.user)
+    user_id = fields.Many2one('hr.department', string='Sales', index=True, track_visibility='onchange', default=lambda self: self.env.user)
