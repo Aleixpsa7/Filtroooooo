d75ad32694a0ffb5e45481e058d4e05e5a0f255b
psat6 <psat6@penguin>
2021-03-01 10:50:41 +0100

    1.7

--- models/models.py
+++ models/models.py
@@ -4,13 +4,8 @@ from odoo import models, fields, api
 
 class hremployee(models.Model):
     _inherit = 'hr.employee'
-    user_id = fields.Many2one('hr.employee', string='Sales', default=lambda self: self.env.user)
+    department_id = fields.Many2one('hr.department', 'Department')
 
-class hrdepartment(models.Model):
-    _inherit = 'hr.department'
-    department_id = fields.Many2one('hr.department', string='Department')
-    member_ids = fields.One2many('hr.employee', 'department_id', string='Members', readonly=True)
-
-class resusers(models.Model):
-    _inherit = 'res.users'
-    
+class respartner(models.Model):
+    _inherit = 'res.partner'
+    sales_id = fields.Many2one('res.partner', string='Sales', domain=[('department_id','=', 'Sales')])
