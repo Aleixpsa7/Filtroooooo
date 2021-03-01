4d2ed03706100f133c1af44158c0dd49f369db87
psat6 <psat6@penguin>
2021-03-01 11:44:52 +0100

    1.8

--- models/models.py
+++ models/models.py
@@ -3,5 +3,5 @@
 from odoo import models, fields, api
 
 class filtro(models.Model):
-    _name: 'res.partner'
+    _name: 'filtro.sales'
     sales_id = fields.Many2one('res.partner', string='Sales', domain=[('department_id.name','=', 'Sales')])
