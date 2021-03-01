d7eaf1374a685ab35466fa5e8a6e4ce9b8e1a150
psat6 <psat6@penguin>
2021-03-01 11:41:58 +0100

    1.8

--- models/models.py
+++ models/models.py
@@ -3,6 +3,5 @@
 from odoo import models, fields, api
 
 class filtro(models.Model):
-    _name: 'filtro.sales'
-    _inherits = ['res.partner','hr.employee', 'hr.department']
+    _name: 'res.partner'
     sales_id = fields.Many2one('res.partner', string='Sales', domain=[('department_id.name','=', 'Sales')])
